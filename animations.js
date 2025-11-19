9f455c12ae5737a931f0dbe8a89c1783d232f1d8
izhank216 <izhank216@gmail.com>
2025-11-19 15:55:10 +0500

    Update animations.js

--- animations.js
+++ animations.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 import 'jquery-ui-dist/jquery-ui';
 import 'jquery-migrate';
 import { gsap } from 'gsap';
-import anime from 'animejs/dist/bundles/anime.umd.min.js';
 
 // jQuery animations
 export const fadeInElement = (s, d = 400) => $(s).fadeIn(d);
@@ -14,20 +13,3 @@ export const animateTweets = (s) => $(s).hide().slideDown(600);
 export const gsapFadeIn = (s, d = 1) => gsap.from(s, { opacity: 0, duration: d });
 export const gsapSlideUp = (s, y = 50, d = 1) =>
   gsap.from(s, { y, opacity: 0, duration: d });
-
-// Anime.js animations
-export const animeBounce = (s, d = 800) =>
-  anime({
-    targets: s,
-    translateY: [-10, 0],
-    easing: 'easeOutBounce',
-    duration: d,
-  });
-
-export const animeRotate = (s, d = 1000) =>
-  anime({
-    targets: s,
-    rotate: '1turn',
-    duration: d,
-    easing: 'easeInOutSine',
-  });
